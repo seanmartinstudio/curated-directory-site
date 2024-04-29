1dc04b01355c1322afc6f5d2981a03f11187cb0c
Sean Martin <sean@seanmartinstudio.com>
2024-04-29 15:47:28 -0700

    refactos script

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,8 +1,19 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-    const element = document.querySelector('.nav-links');
-    if (element.style.display === "none") {
+// document.querySelector('.hamburger').addEventListener('click', () => {
+//     const element = document.querySelector('.nav-links');
+//     if (element.style.display === "none") {
+//       element.style.display = "grid";
+//   } else {
+//       element.style.display = "none";
+//   }
+//   });
+
+document.querySelector('.hamburger').addEventListener('touchstart', (event) => {
+  event.preventDefault(); // Prevents default touch behavior (like scrolling)
+
+  const element = document.querySelector('.nav-links');
+  if (element.style.display === "none") {
       element.style.display = "grid";
   } else {
       element.style.display = "none";
   }
-  });
+});
